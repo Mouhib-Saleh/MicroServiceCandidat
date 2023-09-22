fac525b51597dd69f1883e8ebded6807f1e51b6d
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-09-22 13:42:20 +0100

    q10 done

--- src/main/java/com/esprit/microservice/candidate/controllers/CandidatRestAPI.java
+++ src/main/java/com/esprit/microservice/candidate/controllers/CandidatRestAPI.java
@@ -1,13 +1,38 @@
 package com.esprit.microservice.candidate.controllers;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.esprit.microservice.candidate.entities.Candidat;
+import com.esprit.microservice.candidate.services.CandidatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class CandidatRestAPI {
-     private String title="hello , im the candidate micro-service";
+     private String title="hello , im the candidate microservice";
 
      @RequestMapping("/hello")
      public String sayHello(){
          System.out.println(title);
          return title;
      }
+    @Autowired
+    private CandidatService candidatService;
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat) {
+        return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.OK);
+    }
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Candidat> updateCandidat(@PathVariable(value = "id") int id,
+                                                   @RequestBody Candidat candidat){
+        return new ResponseEntity<>(candidatService.updateCandidat(id, candidat),
+                HttpStatus.OK);
+    }
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteCandidat(@PathVariable(value = "id") int id){
+        return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
+    }
 }
